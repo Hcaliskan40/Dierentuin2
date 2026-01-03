39c43a5cf0002c6b2070cc88657b3eaf66e3f2a4
Hcaliskan <52010720+Hcaliskan40@users.noreply.github.com>
2026-01-03 12:50:40 +0100

    animals

--- ZooManager/obj/Debug/net8.0/ZooManager.AssemblyInfo.cs
+++ ZooManager/obj/Debug/net8.0/ZooManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZooManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+479f7dbd2c8ca474d2ed4c34122ae726998fd3fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2db8f9caf32c13d5f3ad6d8713b0946a52c29378")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZooManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZooManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
