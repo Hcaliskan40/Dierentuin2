ed4ae74f87b61f51c76f888d1aa6c462b9a0d52f
Brandon Hiraki <brandon99@live.nl>
2026-01-03 14:52:10 +0100

    update launch settings for HTTPS and increment assembly informational version

--- ZooManager/obj/Debug/net8.0/ZooManager.AssemblyInfo.cs
+++ ZooManager/obj/Debug/net8.0/ZooManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZooManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c43a9b6ff0ebc9e297f35b33338e971a34ac0dc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c584719e6f829e14e5d3707bceebbc567b2b7d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZooManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZooManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
