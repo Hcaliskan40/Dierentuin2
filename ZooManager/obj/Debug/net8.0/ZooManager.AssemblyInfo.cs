aaa8d83345cf20cb1182c861a11d3b01136d54ff
Brandon Hiraki <brandon99@live.nl>
2026-01-03 12:38:03 +0100

    update assembly informational version and NuGet tool version

--- ZooManager/obj/Debug/net8.0/ZooManager.AssemblyInfo.cs
+++ ZooManager/obj/Debug/net8.0/ZooManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZooManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7789cb3d47e43457a9a6ce4d39a2060d366fa2ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46328715e9e56748a10b1b3115e48cbf934060ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZooManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZooManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
